1566af5dec1c66064d8ef546efdf94260363cedb
Akachukwu Ajulibe <akachukwuajulibe@Akachukwus-MacBook-Pro.local>
2021-02-12 16:43:31 +0100

    finished framework

--- App.tsx
+++ App.tsx
@@ -1,21 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
-
-import Navigation from './src/navigation/MainNavigator';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import AppLoading from "expo-app-loading";
+import * as Font from "expo-font";
+import Navigation from "./src/navigation/MainNavigator";
 
 export default function App() {
-  // const isLoadingComplete = useCachedResources();
-  // const colorScheme = useColorScheme();
+  const fetchFonts = () => {
+    return Font.loadAsync({
+      "Roboto-Black": require("./src/assets/fonts/Roboto-Black.ttf"),
+      "Roboto-BlackItalic": require("./src/assets/fonts/Roboto-BlackItalic.ttf"),
+      "Roboto-Bold": require("./src/assets/fonts/Roboto-Bold.ttf"),
+      "Roboto-BoldItalic": require("./src/assets/fonts/Roboto-BoldItalic.ttf"),
+      "Roboto-Italic": require("./src/assets/fonts/Roboto-Italic.ttf"),
+      "Roboto-Light": require("./src/assets/fonts/Roboto-Light.ttf"),
+      "Roboto-LightItalic": require("./src/assets/fonts/Roboto-LightItalic.ttf"),
+      "Roboto-Medium": require("./src/assets/fonts/Roboto-Medium.ttf"),
+      "Roboto-MediumItalic": require("./src/assets/fonts/Roboto-MediumItalic.ttf"),
+      "Roboto-Regular": require("./src/assets/fonts/Roboto-Regular.ttf"),
+      "Roboto-Thin": require("./src/assets/fonts/Roboto-Thin.ttf"),
+      "Roboto-ThinItalic": require("./src/assets/fonts/Roboto-ThinItalic.ttf"),
+    });
+  };
 
-  // if (!isLoadingComplete) {
-  //   return null;
-  // } else {
+  const [fontLoaded, setFontLoaded] = useState(false);
+
+  if (!fontLoaded) {
     return (
-      <SafeAreaProvider>
-        <Navigation/>
-        <StatusBar />
-      </SafeAreaProvider>
+      <AppLoading
+        startAsync={fetchFonts}
+        onFinish={() => {
+          setFontLoaded(true);
+        }}
+        onError={console.warn}
+      />
     );
-  // }
+  }
+  return (
+    <SafeAreaProvider>
+      <Navigation />
+      <StatusBar />
+    </SafeAreaProvider>
+  );
 }
--- src/screens/splashscreen/SplashScreen.tsx
+++ src/screens/splashscreen/SplashScreen.tsx
@@ -1,22 +1,74 @@
 import React, { useEffect } from "react";
-import { View, StyleSheet, Image } from "react-native";
+import { View, StyleSheet, Text, Image } from "react-native";
+import { Fontisto } from "@expo/vector-icons";
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from "react-native-responsive-screen";
 
 const SplashScreen: React.FC = ({ navigation }: any) => {
   useEffect(() => {
     setTimeout(() => {
-      navigation.navigate("GetStarted");
+      navigation.navigate("Authentication");
     }, 3000);
   });
 
   return (
-    <View >
-        
+    <View style={styles.container}>
+      <View style={styles.messageDiv}>
+        <Image
+          source={require("../../assets/images/wallet.png")}
+          style={styles.walletImage}
+        />
+        <Text style={styles.loading}>Loading...</Text>
+        <Text style={styles.message}>
+          We are creating your SurePay account. Please give us a moment.
+        </Text>
+        <Image
+          source={require("../../assets/images/spinner.gif")}
+          style={styles.spinner}
+        />
+      </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-    
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#DB3900",
+  },
+  loading: {
+    fontSize: hp("3.35%"),
+    color: "white",
+    fontFamily: "Roboto-Bold",
+    marginTop: hp("3.35%"),
+    lineHeight: hp("3.79%"),
+  },
+  message: {
+    fontSize: hp("2%"),
+    color: "white",
+    textAlign: "center",
+    marginTop: hp("1.12%"),
+    fontFamily: "Roboto-Regular",
+    lineHeight: hp("2%"),
+  },
+  messageDiv: {
+    justifyContent: "center",
+    alignItems: "center",
+    width: wp("86.95%"),
+  },
+  walletImage: {
+    width: wp("19.28%"),
+    height: hp("8.8%"),
+  },
+  spinner: {
+    width: wp("9.66%"),
+    height: hp("4.46%"),
+    marginTop: hp("4.46%"),
+  },
 });
 
 export default SplashScreen;
